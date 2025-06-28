7a9e441cf85d480c3f922efb52e3da8a5750e40e
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-06-27 18:59:23 -0700

    Add USDT environment variable check in datacrumbs_init function

--- src/datacrumbs/library.c
+++ src/datacrumbs/library.c
@@ -1,11 +1,17 @@
 
 #include "library.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 __attribute__((visibility("default"))) void datacrumbs_start() {}
 __attribute__((visibility("default"))) void datacrumbs_stop() {}
 
 void datacrumbs_init(void) {
     datacrumbs_start();
+    char *env = getenv("DATACRUMBS_USDT_ENABLE");
+    if (env == NULL || strcmp(env, "1") != 0) {
+        return;
+    }
     sleep(15);
 }
 
