01496da7e64588776d28c6266824699271701bd7
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-07-30 16:35:24 -0700

    Refactor code structure for improved readability and maintainability

--- poc/bpf-core/example.bpf.c
+++ poc/bpf-core/example.bpf.c
@@ -21,20 +21,52 @@ struct {
  __type(value, struct user_msg_t);
 } my_config SEC(".maps");
 SEC("ksyscall/execve")
-int BPF_KPROBE_SYSCALL(hello, const char *pathname) {
- struct data_t data= {};
- struct user_msg_t *p;
- data.pid= bpf_get_current_pid_tgid() >> 32;
- data.uid= bpf_get_current_uid_gid() & 0xFFFFFFFF;
- bpf_get_current_comm(&data.command, sizeof(data.command));
- bpf_probe_read_user_str(&data.path, sizeof(data.path), pathname);
- p= bpf_map_lookup_elem(&my_config, &data.uid);
- if (p != 0) {
-  bpf_probe_read_kernel_str(&data.message, sizeof(data.message), p->message);
- } else {
-  bpf_probe_read_kernel_str(&data.message, sizeof(data.message), message);
- }
- bpf_perf_event_output(ctx, &output, BPF_F_CURRENT_CPU, &data, sizeof(data));
+int BPF_KPROBE(hello, struct pt_regs *regs) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing function enter %s PID %d", pid);
+ //  struct data_t data= {};
+ //  struct user_msg_t *p;
+ //  data.pid= pid;
+ //  data.uid= bpf_get_current_uid_gid() & 0xFFFFFFFF;
+ //  bpf_get_current_comm(&data.command, sizeof(data.command));
+ //  bpf_probe_read_user_str(&data.path, sizeof(data.path), pathname);
+ //  p= bpf_map_lookup_elem(&my_config, &data.uid);
+ //  if (p != 0) {
+ //   bpf_probe_read_kernel_str(&data.message, sizeof(data.message), p->message);
+ //  } else {
+ //   bpf_probe_read_kernel_str(&data.message, sizeof(data.message), message);
+ //  }
+ //  bpf_perf_event_output(ctx, &output, BPF_F_CURRENT_CPU, &data, sizeof(data));
+ return 0;
+}
+SEC("kretsyscall/execve")
+int BPF_KRETPROBE(hello3, struct pt_regs *regs) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing execve exit PID %d", pid);
+ return 0;
+}
+SEC("kprobe/write_inode_now")
+int BPF_KPROBE(write_inode_now2) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing write_inode entry PID %d", pid);
+ return 0;
+}
+SEC("kretprobe/write_inode_now")
+int BPF_KRETPROBE(write_inode_now3, struct pt_regs *regs) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing write_inode exit PID %d", pid);
+ return 0;
+}
+SEC("uprobe/usr/lib64/libc.so.6:printf")
+int BPF_UPROBE(printf23, struct pt_regs *regs) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing printf entry PID %d", pid);
+ return 0;
+}
+SEC("uretprobe/usr/lib64/libc.so.6:printf")
+int BPF_URETPROBE(printf2, struct pt_regs *regs) {
+ u32 pid= bpf_get_current_pid_tgid() >> 32;
+ bpf_trace_printk("Tracing printf exit PID %d", pid);
  return 0;
 }
 char LICENSE[] SEC("license")= "Dual BSD/GPL";
