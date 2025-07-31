b5ed49666e0c2d5657e023d2c86312e6dd59a91d
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-07-31 16:07:25 -0700

    Refactor BPF code structure and add notes for kernel symbols

--- poc/bpf-core/example.bpf.c
+++ poc/bpf-core/example.bpf.c
@@ -2,71 +2,73 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/usdt.bpf.h>
 //
 #include "example.h"
 //
 char message[12]= "Hello World";
-struct {
- __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
- __uint(key_size, sizeof(u32));
- __uint(value_size, sizeof(u32));
-} output SEC(".maps");
-struct user_msg_t {
- char message[12];
-};
-struct {
- __uint(type, BPF_MAP_TYPE_HASH);
- __uint(max_entries, 10240);
- __type(key, u32);
- __type(value, struct user_msg_t);
-} my_config SEC(".maps");
-SEC("ksyscall/execve")
-int BPF_KPROBE(hello, struct pt_regs *regs) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing function enter %s PID %d", pid);
- //  struct data_t data= {};
- //  struct user_msg_t *p;
- //  data.pid= pid;
- //  data.uid= bpf_get_current_uid_gid() & 0xFFFFFFFF;
- //  bpf_get_current_comm(&data.command, sizeof(data.command));
- //  bpf_probe_read_user_str(&data.path, sizeof(data.path), pathname);
- //  p= bpf_map_lookup_elem(&my_config, &data.uid);
- //  if (p != 0) {
- //   bpf_probe_read_kernel_str(&data.message, sizeof(data.message), p->message);
- //  } else {
- //   bpf_probe_read_kernel_str(&data.message, sizeof(data.message), message);
- //  }
- //  bpf_perf_event_output(ctx, &output, BPF_F_CURRENT_CPU, &data, sizeof(data));
+// anonymous struct assigned to rb variable
+struct
+{
+ // specify the type, eBPF specific syntax
+ __uint(type, BPF_MAP_TYPE_RINGBUF);
+ // specify the size of the buffer
+ // has to be a multiple of the page size
+ __uint(max_entries, 1024 * 1024);
+} output SEC(".maps") /* placed in maps section */;
+int generic_call(int event_id) {
+ struct event_t *evt;
+ evt= bpf_ringbuf_reserve(&output, sizeof(*evt), 0);
+ if (!evt) {
+  bpf_printk("Failed to reserve space in ring buffer\n");
+  return 0;
+ }
+ unsigned long id= bpf_get_current_pid_tgid();
+ int pid= id;
+ evt->pid= pid;
+ evt->event_id= event_id;
+ bpf_ringbuf_submit(evt, 0);
+ bpf_printk("Tracing function %d PID %d", event_id, pid);
  return 0;
 }
-SEC("kretsyscall/execve")
-int BPF_KRETPROBE(hello3, struct pt_regs *regs) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing execve exit PID %d", pid);
+SEC("ksyscall/openat")
+int BPF_KPROBE(openat_entry, struct pt_regs *regs) {
+ generic_call(1);
  return 0;
 }
-SEC("kprobe/write_inode_now")
-int BPF_KPROBE(write_inode_now2) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing write_inode entry PID %d", pid);
+SEC("kretsyscall/openat")
+int BPF_KRETPROBE(openat_exit, struct pt_regs *regs) {
+ generic_call(2);
  return 0;
 }
-SEC("kretprobe/write_inode_now")
-int BPF_KRETPROBE(write_inode_now3, struct pt_regs *regs) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing write_inode exit PID %d", pid);
+SEC("kprobe/vfs_write")
+int BPF_KPROBE(vfs_write_entry) {
+ generic_call(3);
  return 0;
 }
-SEC("uprobe/usr/lib64/libc.so.6:printf")
-int BPF_UPROBE(printf23, struct pt_regs *regs) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing printf entry PID %d", pid);
+SEC("kretprobe/vfs_write")
+int BPF_KRETPROBE(vfs_write_exit, struct pt_regs *regs) {
+ generic_call(4);
  return 0;
 }
-SEC("uretprobe/usr/lib64/libc.so.6:printf")
-int BPF_URETPROBE(printf2, struct pt_regs *regs) {
- u32 pid= bpf_get_current_pid_tgid() >> 32;
- bpf_trace_printk("Tracing printf exit PID %d", pid);
+SEC("uprobe//usr/lib64/libc.so.6:open")
+int BPF_UPROBE(open_entry, struct pt_regs *regs) {
+ generic_call(5);
+ return 0;
+}
+SEC("uretprobe//usr/lib64/libc.so.6:open")
+int BPF_URETPROBE(open_exit, struct pt_regs *regs) {
+ generic_call(6);
+ return 0;
+}
+SEC("usdt//usr/lib64/libpython3.11.so:python:function__entry")
+int BPF_USDT(python_function_entry, void *class, void *function) {
+ generic_call(7);
+ return 0;
+}
+SEC("usdt//usr/lib64/libpython3.11.so:python:function__return")
+int BPF_USDT(python_function_return, void *class, void *function) {
+ generic_call(8);
  return 0;
 }
 char LICENSE[] SEC("license")= "Dual BSD/GPL";
--- poc/bpf-core/example.c
+++ poc/bpf-core/example.c
@@ -1,73 +1,61 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <bpf/libbpf.h>
 #include "example.h"
 #include "example.skel.h"
-
-static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
-{
-	if (level >= LIBBPF_DEBUG)
-		return 0;
-
-	return vfprintf(stderr, format, args);
+#include <bpf/libbpf.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
+ if (level >= LIBBPF_DEBUG)
+  return 0;
+ return vfprintf(stderr, format, args);
 }
-
-void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz)
-{
-	struct data_t *m = data;
-
-	printf("%-6d %-6d %-16s %-16s %s\n", m->pid, m->uid, m->command, m->path, m->message);
+int handle_event(void *ctx, void *data, size_t data_sz) {
+ struct event_t *m= data;
+ if (m->pid == 0 || m->event_id == 0) {
+  return 0;
+ }
+ printf("%-6d  %-6u\n", m->pid, m->event_id);
+ return 0;
 }
-
-void lost_event(void *ctx, int cpu, long long unsigned int data_sz)
-{
-	printf("lost event\n");
+void lost_event(void *ctx, int cpu, long long unsigned int data_sz) {
+ printf("lost event\n");
 }
-
-int main()
-{
-    struct example_bpf *skel;
-    int err;
-	struct perf_buffer *pb = NULL;
-
-	libbpf_set_print(libbpf_print_fn);
-
-	skel = example_bpf__open_and_load();
-	if (!skel) {
-		printf("Failed to open BPF object\n");
-		return 1;
-	}
-
-	err = example_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton: %d\n", err);
-		example_bpf__destroy(skel);
-        return 1;
-	}
-
-	pb = perf_buffer__new(bpf_map__fd(skel->maps.output), 8, handle_event, lost_event, NULL, NULL);
-	if (!pb) {
-		err = -1;
-		fprintf(stderr, "Failed to create ring buffer\n");
-		example_bpf__destroy(skel);
-        return 1;
-	}
-
-	while (true) {
-		err = perf_buffer__poll(pb, 100 /* timeout, ms */);
-		// Ctrl-C gives -EINTR
-		if (err == -EINTR) {
-			err = 0;
-			break;
-		}
-		if (err < 0) {
-			printf("Error polling perf buffer: %d\n", err);
-			break;
-		}
-	}
-
-	perf_buffer__free(pb);
-	example_bpf__destroy(skel);
-	return -err;
+int main() {
+ struct example_bpf *skel;
+ int err;
+ struct ring_buffer *rb= NULL;
+ libbpf_set_print(libbpf_print_fn);
+ skel= example_bpf__open_and_load();
+ if (!skel) {
+  printf("Failed to open BPF object\n");
+  return 1;
+ }
+ err= example_bpf__attach(skel);
+ if (err) {
+  fprintf(stderr, "Failed to attach BPF skeleton: %d\n", err);
+  example_bpf__destroy(skel);
+  return 1;
+ }
+ rb= ring_buffer__new(bpf_map__fd(skel->maps.output), handle_event, NULL, NULL);
+ if (!rb) {
+  err= -1;
+  fprintf(stderr, "Failed to create ring buffer\n");
+  example_bpf__destroy(skel);
+  return 1;
+ }
+ while (true) {
+  err= ring_buffer__poll(rb, 30000);
+  // Ctrl-C gives -EINTR
+  if (err == -EINTR) {
+   err= 0;
+   break;
+  }
+  if (err < 0) {
+   printf("Error polling ring buffer: %d\n", err);
+   break;
+  }
+ }
+ ring_buffer__free(rb);
+ example_bpf__destroy(skel);
+ return -err;
 }
