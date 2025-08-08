12e5cffc5a7c9fbe71a5fe58f7cfa30987da774b
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-08-07 19:54:14 -0700

    Refactor probe generation and handling for fork-related functions (#41)
    - Updated the `probes-haridev-lead.json` to include fork-related functions in the libc probe list.
    - Modified `test_dlio_benchmark.sh` to streamline the usage of LD_PRELOAD.
    - Removed hardcoded probe names for fork and vfork from `generator.cpp` to simplify event updates.
    - Enhanced `kprobe_generator.h`, `syscall_generator.h`, and `uprobe_generator.h` to differentiate exit functions for fork-related probes.
    - Introduced `generic_fork_exit` function in `common.h` to handle tracing for fork events.
    - Cleaned up `init.bpf.c` by removing redundant fork and vfork syscall probes, relying on the new generic exit handling.

--- scripts/lead2/client/tests/test_dlio_benchmark.sh
+++ scripts/lead2/client/tests/test_dlio_benchmark.sh
@@ -6,7 +6,5 @@ PYTHON_DIR=/usr/bin/python3.9 #$(spack location -i python@3.10.16)
 echo "$(date '+%Y-%m-%d %H:%M:%S') Run Py test ${PYTHON_DIR}"
 cmd=(dlio_benchmark workload=unet3d_a100.yaml ++workload.dataset.data_folder=/tmp/dlio/data/unet3d ++workload.workflow.train=True ++workload.workflow.generate_data=False  ++workload.checkpoint.checkpoint_folder=/tmp/dlio/checkpoint/unet3d ++workload.train.epochs=1)
 echo "${cmd[@]}"
-export LD_PRELOAD=${DATACRUMBS_SO}
-"${cmd[@]}"
-unset LD_PRELOAD
+LD_PRELOAD=${DATACRUMBS_SO} "${cmd[@]}"
 echo "$(date '+%Y-%m-%d %H:%M:%S') Finished Py test"
