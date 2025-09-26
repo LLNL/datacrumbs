7d5a2cb0a97a16017b477957c0cd4362217c04a7
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-09-25 21:24:04 -0700

    feat: Add formatting checks and autoformat scripts for C/C++ code

--- src/datacrumbs/server/bpf/common.h
+++ src/datacrumbs/server/bpf/common.h
@@ -17,8 +17,7 @@ DATACRUMBS_MAP_EXTERN(fn_pid_map, struct fn_key_t, struct fn_value_t);
 
 #if defined(DATACRUMBS_MODE) && (DATACRUMBS_MODE == 1)
 DATACRUMBS_MAP_EXTERN(failed_request, u32, u32, 128);
-DATACRUMBS_RINGBUF_EXTERN(output,
-                          1024 * 1024U * DATACRUMBS_TRACE_RINGBUF_SIZE_MB);
+DATACRUMBS_RINGBUF_EXTERN(output, 1024 * 1024U * DATACRUMBS_TRACE_RINGBUF_SIZE_MB);
 #else
 DATACRUMBS_MAP_EXTERN(profile, struct profile_key_t, struct profile_value_t, 1024);
 DATACRUMBS_MAP_EXTERN(usdt_profile, struct usdt_profile_key_t, struct profile_value_t, 1024);
