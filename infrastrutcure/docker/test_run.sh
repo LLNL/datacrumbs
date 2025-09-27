581d14e550b26baa82c7113277f199c17c78a7b4
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-09-26 23:30:36 -0700

    Fixes CI Test
    Now Datacrumbs exits after it starts correctly and automatically demonizes.

--- infrastrutcure/docker/test_run.sh
+++ infrastrutcure/docker/test_run.sh
@@ -2,9 +2,7 @@
 /opt/datacrumbs-build/bin/datacrumbs start docker --user root \
                     --config_path /opt/datacrumbs/etc/datacrumbs/configs \
                     --data_dir /opt/datacrumbs/etc/datacrumbs/data \
-                    --trace_log_dir /opt/datacrumbs/etc/datacrumbs/logs &
-DATACRUMBS_PID=$!
-sleep 60
+                    --trace_log_dir /opt/datacrumbs/etc/datacrumbs/logs
 mkdir -p /opt/data/
 rm -rf /opt/data/*
 LD_PRELOAD=/opt/datacrumbs-build/lib64/libdatacrumbs_client.so dd if=/dev/zero of=/opt/data/img_temp.bat bs=1M count=16
