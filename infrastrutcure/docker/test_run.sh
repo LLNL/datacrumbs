1ec05b82208ac679f478216c42708223fc95592f
Hariharan Devarajan <mani.hariharan@gmail.com>
2025-11-01 23:36:19 -0700

    Provide enhanced security of scripts

--- infrastrutcure/docker/test_run.sh
+++ infrastrutcure/docker/test_run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-/opt/datacrumbs-build/bin/datacrumbs start docker --user root \
-                    --config_path /opt/datacrumbs/etc/datacrumbs/configs \
-                    --data_dir /opt/datacrumbs/etc/datacrumbs/data \
-                    --trace_log_dir /opt/datacrumbs/etc/datacrumbs/logs
-mkdir -p /opt/data/
-rm -rf /opt/data/*
-LD_PRELOAD=/opt/datacrumbs-build/lib64/libdatacrumbs_client.so dd if=/dev/zero of=/opt/data/img_temp.bat bs=1M count=16
-/opt/datacrumbs-build/bin/datacrumbs stop docker --user root \
-                    --config_path /opt/datacrumbs/etc/datacrumbs/configs \
-                    --data_dir /opt/datacrumbs/etc/datacrumbs/data \
-                    --trace_log_dir /opt/datacrumbs/etc/datacrumbs/logs
-zcat /opt/datacrumbs/etc/datacrumbs/logs/*
-exit 0
+set -x
+source /opt/datacrumbs-install/bin/datacrumbs_setup --verbose
+datacrumbs_create_log_dir --verbose
+datacrumbs_run --verbose --app "datacrumbs_wrap dd if=/dev/zero of=/tmp/img_temp.bat bs=1M count=16"
